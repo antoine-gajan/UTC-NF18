29053b144a886942bca7a135c57ba7fbf4e52cfb
Antoine Gajan <antoine.gajan@etu.utc.fr>
2022-05-03 07:16:48 +0000

    Upload New File

---
+++ connexion.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+
+def connexion():
+    """Fonction permettant de se connecter à la BDD"""
+    #Identifiant de connexion
+    HOST = "tuxa.sme.utc"
+    USER = "nf18p054"
+    PASSWORD = "YktA3UMv"
+    DATABASE = "dbnf18p054"
+
+    #Ouverture de la connexion
+    conn = psycopg2.connect(f"host={HOST} dbname={DATABASE} user={USER} password={PASSWORD}")
+
+    #Création d'un curseur
+    cur = conn.cursor()
+    return conn, cur
+
+def close(connexion):
+    """Fonction qui ferme la connexion avec la BDD"""
+    connexion.close()
