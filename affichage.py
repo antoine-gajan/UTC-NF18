f0b8b7060b14cc4d57376f8c23b724b3f265a8c2
Antoine Gajan <antoine.gajan@etu.utc.fr>
2022-05-03 07:16:04 +0000

    Upload New File

---
+++ affichage.py
@@ -0,0 +1,8 @@
+def afficheMenuPrincipal():
+    """Fonction qui affiche le menu principal"""
+    print("======= GESTION DE COMPTE BANCAIRE =======")
+    print("""1. Insertion\n2. Modification\n3. Suppression\n4. Affichage de la base de données\nRequêtes :\n5. Montant d'un compte\n6. Nombre de chèques émis par un client\n7. Quitter""")
+
+def afficheMenuInsertion():
+    print("*** Insertion de données ***")
+    print("""1. Ajout d'un client\n2. Création d'un compte\n3. Ajout d'une opération sur un compte""")
