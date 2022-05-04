231eaf7665a46a8fc82a58d7890532da4eef4f87
Antoine Gajan <antoine.gajan@etu.utc.fr>
2022-05-04 11:06:28 +0000

    Upload New File

---
+++ suppression.py
@@ -0,0 +1,21 @@
+def supprimerClient(cur):
+    """Fonction qui supprime un client de la BDD"""
+    numero = input("Numéro de téléphone du client : ")
+    sql = f"DELETE FROM Client WHERE telephone = {numero}"
+    try:
+        cur.execute(sql)
+        cur.commit()
+    except:
+        cur.rollback()
+        print("Erreur rencontréE. Impossible de supprimer ce client.")
+
+def supprimerCompte(cur):
+    """Fonction qui supprime un compte de la BDD"""
+    date_creation = input("Date de création du compte : ")
+    sql = f"""DELETE FROM Client WHERE date_creation = TO_DATE({date_creation}, "YYYY-MM-DD")"""
+    try:
+        cur.execute(sql)
+        cur.commit()
+    except:
+        cur.rollback()
+        print("Erreur rencontrée. Impossible de supprimer ce compte.")
