143e14ea1fe0b9d0890abc2fc960e28249155d46
Theo Dubus <theo.dubus@etu.utc.fr>
2022-05-04 11:12:01 +0000

    Update requete.py

--- requete.py
+++ requete.py
@@ -1,4 +1,4 @@
-
+from datetime import date
 
 def getClients(curseur):
     """Fonction qui renvoie l'ensemble des clients de la BDD"""
@@ -73,6 +73,65 @@ if choix == 'O':
 sql = f"SELECT SUM(Operation.montant) FROM Opération INNER JOIN Client ON Opération.client = Client.id WHERE Client.téléphone = TELEPHONE AND Opération.TypeOpération = TypeOpération"
     montant = curseur.execute(sql)
     return montant
+
+
+def Typecompte(curseur, date_creation):
+    """Fonction qui détermine le type d'un compte"""
+    sql1 = f"SELECT * FROM Compte JOIN CompteCourant ON CompteCourant.compte = Compte.date_creation WHERE date_creation = {date_creation}"
+    sql2 = f"SELECT * FROM Compte JOIN CompteRevolving ON CompteRevolving.compte = Compte.date_creation WHERE date_creation = {date_creation}"
+    compte = curseur.execute(sql1)
+    if len(compte) == 0:
+        compte = curseur.execute(sql2)
+        if len(compte) == 0:
+            return "epargne"
+        else:
+            return "revolving"
+    return "courant"
+
+def GetMin(curseur, date_creation):
+    """Fonction qui rentourne le min autorsé d'un compte revolving"""
+    sql = f"SELECT montant_min FROM CompteRevolving WHERE date_creation = {date_creation}"
+    compte = curseur.execute(sql)
+    return compte[0][0]
+
+def GetDecouvert(curseur, date_creation):
+    """Fonction qui rentourne le découvert autorsé d'un compte courant"""
+    sql = f"SELECT decouvert_autorise FROM CompteCourant WHERE date_creation = {date_creation}"
+    compte = curseur.execute(sql)
+    return compte[0][0]
+
+def UpdateMinMaxMois(curseur, date_creation):
+    """Fonction qui met à jour le min/max du mois d'un compte, et qui créé une nouvelle isntance si on a changé de mois"""
+    sql = f"SELECT * FROM MinMaxMois WHERE compte = {date_creation} AND annee = {date.today().year} AND mois = {date.today().month}"
+    MinMax = curseur.execute(sql)
+
+    # Pas d'instance pour ce mois ci, on la crée
+    if (len(MinMax) == 0):
+        sql = f"INSERT INTO MinMaxMois (annee, min, max, mois, compte) VALUES ({date.today().year}, {getSoldeCompte(curseur, date_creation)[0][0]}, {getSoldeCompte(curseur, date_creation)[0][0]}, {date.today().month}, {date_creation})"
+        try:
+            curseur.execute(sql)
+            curseur.commit()
+        except:
+            curseur.rollback()
+
+    # Déjà une instance pour ce mois ci, on la met a jour si nécéssaire
+    else:
+        if (getSoldeCompte(curseur, date_creation)[0][0] > MinMax[2]):
+            sql = f"UPDATE MinMaxMois SET max = {getSoldeCompte(curseur, date_creation)[0][0]} WHERE compte = {date_creation} AND annee = {date.today().year} AND mois = {date.today().month}"
+            try:
+                curseur.execute(sql)
+                curseur.commit()
+            except:
+                curseur.rollback()
+
+        if (getSoldeCompte(curseur, date_creation)[0][0] < MinMax[1]):
+            sql = f"UPDATE MinMaxMois SET min = {getSoldeCompte(curseur, date_creation)[0][0]} WHERE compte = {date_creation} AND annee = {date.today().year} AND mois = {date.today().month}"
+            try:
+                curseur.execute(sql)
+                curseur.commit()
+            except:
+                curseur.rollback()
+
 
 
 
